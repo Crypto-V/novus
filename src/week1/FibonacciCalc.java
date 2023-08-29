015bce72c4ba64105c40b37533f36790793aa6cc
Crypto-V <verejanvasile92@gmail.com>
2023-08-29 09:55:53 +0100

    implemented using BigInteger

--- src/week1/FibonacciCalc.java
+++ src/week1/FibonacciCalc.java
@@ -1,26 +1,26 @@
 package week1;
 
+import java.math.BigInteger;
+
 public class FibonacciCalc {
 
     public static void main(String[] args) {
-        int size = 100; //This is the number of items to be printed, adjust if necessary.
+        int size = 100; // This is the number of items to be printed, adjust if necessary.
         System.out.println("Iterative method starts here");
         fibonacci(size);
 
         System.out.println("\nRecursive method starts here!");
 
         System.out.print("0, 1, ");
-        recursiveFibonacci(size - 2, 0, 1);
+        recursiveFibonacci(size - 2, BigInteger.ZERO, BigInteger.ONE);
     }
 
     private static void fibonacci(int size) {
-
-        int first = 0;
-        int second = 1;
-        int sum;
+        BigInteger first = BigInteger.ZERO;
+        BigInteger second = BigInteger.ONE;
 
         while (size != 0) {
-            sum = first + second;
+            BigInteger sum = first.add(second);
             System.out.print(first + ", ");
             first = second;
             second = sum;
@@ -28,15 +28,10 @@ public class FibonacciCalc {
         }
     }
 
-    private static void recursiveFibonacci(int size, int num1, int num2) {
-
+    private static void recursiveFibonacci(int size, BigInteger num1, BigInteger num2) {
         if (size > 0) {
-
-            // get next fibonacci number by summing
-            int sum = num1 + num2;
+            BigInteger sum = num1.add(num2);
             System.out.print(sum + ", ");
-
-            // call recursive method to print next fibonacci number
             recursiveFibonacci(size - 1, num2, sum);
         }
     }
